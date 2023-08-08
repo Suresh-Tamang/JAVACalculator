441f7a165d8ad62a702999aaeeed13715389512d
Suresh Tamang <sureshjimba3333@gmail.com>
2023-08-08 23:04:10 +0545

    updated

--- Calculator.java
+++ Calculator.java
@@ -1,40 +1,34 @@
+import org.w3c.dom.Text;
+
 import java.awt.*;
 import java.awt.event.*;
 
-public class Calculator {
     public static void main(String[] args) {
         Frame frame = new Frame("Add Numbers");
         Panel panel = new Panel();
         panel.setLayout(new GridLayout(5, 2));
+ 
 
-        // Create components
-        Label num1Label = new Label("number 1: ");
-        TextField num1Field = new TextField(10);
+        Te xtField num1Field = new TextField(10);
 
-        Label num2Label = new Label("number 2: ");
-        TextField num2Field = new TextField(20);
+        Label num2Label = new Label("number 2: "
 
         Button addButton = new Button("Add");
         Label resultLabel = new Label("Result");
 
-        // creating buttons
         Button subButton = new Button("Subtract");
 
-        Button multiButton = new Button("Multiply");
 
-        Button divButton = new Button("Divide");
-
+        
         TextField resultField = new TextField(10);
         resultField.setEditable(false);
 
         // Add components to the panel
-        panel.add(num1Label);
-        panel.add(num1Field);
+
         panel.add(num2Label);
         panel.add(num2Field);
-        panel.add(addButton);
-        panel.add(subButton);
-        panel.add(multiButton);
+
+        pa nel.add(multiButton);
         panel.add(divButton);
         panel.add(resultLabel);
         panel.add(resultField);
@@ -44,69 +38,60 @@ public class Calculator {
         frame.setSize(300, 200);
         frame.setVisible(true);
 
-        // Add action listener to the button
-        addButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        // Add action listener 
+
+             public void actionPerformed(ActionEvent e) {
                 try {
-                    double num1 = Double.parseDouble(num1Field.getText());
-                    double num2 = Double.parseDouble(num2Field.getText());
-                    double result = num1 + num2;
-                    resultField.setText(String.valueOf(result));
-                } catch (NumberFormatException ex) {
+                    double  num1 = Double.parseDouble(num1Field.getText());
+                    double num2
+
+                 } catch (NumberFormatException ex) {
                     System.out.println(ex);
                 }
             }
-        });
-        // for subtraction//
+        }); 
+
         subButton.addActionListener(new ActionListener() {
-            @Override
+            @Override  
             public void actionPerformed(ActionEvent e) {
-                try {
-                    double num1 = Double.parseDouble(num1Field.getText());
+                t     d ouble num1 = Double.parseD ouble(num1Field.getText());
                     double num2 = Double.parseDouble(num2Field.getText());
                     double result = num1 - num2;
                     resultField.setText(String.valueOf(result));
-                } catch (NumberFormatException ex) {
-                    System.out.println(ex);
-                }
+           
+
             }
         });
-        // for Multiplication//
-        multiButton.addActionListener(new ActionListener() {
+
+        multiButton .addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
+                try {  
                     double num1 = Double.parseDouble(num1Field.getText());
-                    double num2 = Double.parseDouble(num2Field.getText());
-                    double result = num1 * num2;
+                      d ouble result = num1 * num2 ;
                     resultField.setText(String.valueOf(result));
                 } catch (NumberFormatException ex) {
                     System.out.println(ex);
-                }
-            }
-        });
-        // for division//
+           
+
+        
         divButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public  void actionPerformed(ActionEvent e) {
                 try {
                     double num1 = Double.parseDouble(num1Field.getText());
-                    double num2 = Double.parseDouble(num2Field.getText());
+                    double num2 = Double . parseDouble(num2Field.getText());
                     double result = num1 / num2;
-                    resultField.setText(String.valueOf(result));
-                } catch (NumberFormatException ex) {
+                  } cat ch (NumberFormatException  ex) {
                     System.out.println(ex);
                 }
             }
         });
-        // close the application when the frame is closed
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
+
                 frame.dispose();
                 System.exit(0);
             }
-        });
+        }); 
+
     }
 }
